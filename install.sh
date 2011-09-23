62b2ee09fc798c8379a0179fbc1c92565eea87a6
Aaron Gallagher <habnabit@gmail.com>
2011-09-23 13:39:32 -0700

    Adding more dotfiles.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+SRC=$(dirname $0)
+DST=$HOME
+LN='echo ln'
+if [ "$1" = "-d" ]; then LN=ln; fi
+if [ "$1" = "-f" ]; then LN='ln -fs'; fi
+install() {
+    $LN -s "$SRC/$1" "$DST/.$(basename $1 | sed 's:^\.::')"
+}
+
+install emacs.d
+install emacs.d/.emacs
+install gitconfig
+install gitignore_global
