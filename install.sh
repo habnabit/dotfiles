bcaade76723877639a453b9dab72af0f1c799909
Aaron Gallagher <habnabit@gmail.com>
2011-09-28 09:15:18 -0700

    Updating oh-my-zsh; corresponding changes to install.sh.

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
+set -e
 SRC=$(dirname $0)
 DST=$HOME
 LN='echo ln'
-if [ "$1" = "-d" ]; then LN=ln; fi
-if [ "$1" = "-f" ]; then LN='ln -fs'; fi
+if [ "$1" = "-d" ]; then LN='ln -v'; fi
+if [ "$1" = "-f" ]; then LN='ln -fsv'; fi
 install() {
     $LN -s "$SRC/$1" "$DST/.$(basename $1 | sed 's:^\.::')"
 }
 
 (cd $SRC; git submodule init && git submodule update)
+(cd $SRC/oh-my-zsh; gcc parse-git-status.c -o parse-git-status)
 mkdir -p $SRC/emacs.d/compy-specific
 touch $SRC/emacs.d/compy-specific/init.el
 touch $SRC/compy-specific.sh
