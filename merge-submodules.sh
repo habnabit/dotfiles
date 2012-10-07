64e25daa32b48d666181d224eac44bc50540ff7a
Aaron Gallagher <habnabit@gmail.com>
2012-10-06 21:58:19 -0700

    Updating merge-submodules.sh

--- merge-submodules.sh
+++ merge-submodules.sh
@@ -5,3 +5,4 @@ git subtree pull --prefix emacs.d/nxhtml --squash git://github.com/habnabit/nxht
 git subtree pull --prefix emacs.d/twittering-mode --squash git://github.com/hayamiz/twittering-mode.git master
 git subtree pull --prefix emacs.d/magit --squash git://github.com/magit/magit.git master
 git subtree pull --prefix emacs.d/flymake --squash git://github.com/illusori/emacs-flymake.git master
+git subtree pull --prefix emacs.d/circe --squash git://github.com/retroj/circe.git master
