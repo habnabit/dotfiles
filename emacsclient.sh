e5f10fa565f661463930ce0f6d5bf4a34bee1e69
Aaron Gallagher <habnabit@gmail.com>
2013-06-24 18:08:28 -0700

    Flesh out emacsclient.sh.

--- emacsclient.sh
+++ emacsclient.sh
@@ -8,22 +8,27 @@ fi
 if [ -z "$remote_emacs_auth" ] || [ ! -e "$remote_emacs_auth" ] || ! nc -z $(sed -n 1p "$remote_emacs_auth"); then
     echo "no emacs server"
     sleep 1
-    exec emacs "$1"
+    exec emacs "$@"
 fi
 
 quote () {
     echo "$1" | sed 's:&:\&\&:g;s:-:\&-:g;s: :\&_:g'
 }
 
-quoted_pwd=$(quote $(pwd))
-quoted_file=$(quote "$1")
-case "$1" in
-    /*) quoted_file_path="$quoted_file";;
-    *)  quoted_file_path="${quoted_pwd}/${quoted_file}";;
-esac
-
 tramp_auth=$(sed -n 2p "$remote_emacs_auth")
 tramp_prefix=$(sed -n 3p "$remote_emacs_auth")
-args=$"${tramp_auth}\n-dir ${tramp_prefix}${quoted_pwd} -file ${tramp_prefix}${quoted_file_path}"
+args=$"${tramp_auth}\n-dir ${tramp_prefix}${quoted_pwd}"
 [ "$nowait" != 0 ] && args="$args -nowait"
+quoted_pwd=$(quote $(pwd))
+
+for file; do
+    quoted_file=$(quote "${file}")
+    case "${file}" in
+        +*) argument="-position ${quoted_file}";;
+        /*) argument="-file ${tramp_prefix}${quoted_file}";;
+        *)  argument="-file ${tramp_prefix}${quoted_pwd}/${quoted_file}";;
+    esac
+    args="${args} $argument"
+done
+
 echo "$args" | nc $(sed -n 1p "$remote_emacs_auth")
