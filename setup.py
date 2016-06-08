aaf3f25505287b495a84e9de2c327daf351ef461
Aaron Gallagher <_@habnab.it>
2016-06-07 23:46:51 -0700

    Squashed 'fish/' content from commit b58dd13
    git-subtree-dir: fish
    git-subtree-split: b58dd13af57510315c0372caf7b3cb67350189eb

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from os import path
+
+long_desc = open(path.join(path.dirname(__file__), "README.rst"), "U").read()
+
+setup(name="fish", version="1.1",
+      url="http://sendapatch.se/",
+      author="Ludvig Ericson",
+      author_email="ludvig@sendapatch.se",
+      description="Animating fish (and birds) for progress bars",
+      long_description=long_desc,
+      use_2to3=True,
+      py_modules=["fish"])
