41a0ec2b0a51f4112ad0cbb4db5506cd07b7a1c3
Aaron Gallagher <_@habnab.it>
2016-07-30 19:05:45 -0700

    Fix install.sh to work.
    It was hella broken.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -eux
 bindir=$(printf "bin-%s-%s" $(uname -sm))
-prompt_utils="oh-my-zsh/helper-bins/${bindir}"
+prompt_utils_desc="oh-my-zsh/helper-bins/${bindir}/hab-prompt-utils"
+prompt_utils_dir="oh-my-zsh/helper-bins/${bindir}-alt"
+mkdir -p "${prompt_utils_dir}"
+prompt_utils="${prompt_utils_dir}/hab-prompt-utils"
+cp "${prompt_utils_desc}" "${prompt_utils}"
 python lfs-fetch.py .lfsconfig "${prompt_utils}"
-"${prompt_utils}" install "$(pwd)/Manifest" "${HOME}"
+chmod +x "${prompt_utils}"
+"${prompt_utils}" install ./Manifest "${HOME}"
